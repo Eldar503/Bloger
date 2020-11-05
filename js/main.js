38f807358166dec10a50b6012b93ae1f7a1011f3
Mokesov Eldar <eldarmol587@gmail.com>
2020-11-05 12:56:10 +0300

    Да я вернулся

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-collapse');
 
-    /** click event on toggle menu */
+    
     $toggleCollapse.click(function () {
         $nav.toggleClass('collapse');
     })
